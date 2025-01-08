953dff52141d38b604a02006e930000a7182876c
Luntic-go <eliza.nikaa@mail.ru>
2025-01-08 11:31:38 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,6 @@
+quantity_completed_homeworks = 12
+hours_spent = 1.5
+course_title = 'Python'
+time_for_one_task = 1.5 / 12
+
+print("Курс: ", course_title, ", всего задач: ", quantity_completed_homeworks, ", затрачено часов: ", hours_spent, ", среднее время выполнения: ", time_for_one_task, " часов.")
