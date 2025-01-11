49efa8d527955217756ef235b374d090f0a4a040
Luntic-go <eliza.nikaa@mail.ru>
2025-01-11 09:01:22 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+my_string = input('Введите текст: ')
+
+print('Количество символов в строке:', len(my_string))
+print('Строка в верхнем регистре:', my_string.upper())
+print('Строка в нижнем регистре:', my_string.lower())
+print('Строка в нижнем регистре:', my_string.replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
